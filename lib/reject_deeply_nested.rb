b054f19597df87f6c7fe7e051fe099a5cdd7b740
Ignat Zakrevsky <iezakrevsky@gmail.com>
2016-01-23 22:26:01 +0300

    Change procs namespace

--- lib/reject_deeply_nested.rb
+++ lib/reject_deeply_nested.rb
@@ -2,18 +2,25 @@ require "reject_deeply_nested/version"
 require "active_support/core_ext/object/blank"
 
 module RejectDeeplyNested
-  class DeeplyNested
-    BLANK = proc { |attributes| deep_blank?(attributes) }
+  DEFAULT_IGNORE_VALUES = [/_destroy/]
 
-    # Recursively traverse nested attributes to define is all values blank.
-    # Additionaly considers that value with _destroy key is always blank.
-    def self.deep_blank?(hash)
-      hash.each do |key, value|
-        next if key == '_destroy'
-        any_blank = value.is_a?(Hash) ? deep_blank?(value) : value.blank?
-        return false unless any_blank
-      end
-      true
+  BLANK = proc { |attributes| deep_blank?(attributes) }
+  SMART_BLANK = proc do |ignore_values, attributes|
+    deep_blank?(attributes, Array(ignore_values) + DEFAULT_IGNORE_VALUES)
+  end
+
+  private
+
+  # Recursively traverse nested attributes to define is all values blank.
+  # Additionaly considers that value with _destroy key is always blank.
+  def self.deep_blank?(hash, ignore_values = DEFAULT_IGNORE_VALUES)
+    hash.each do |key, value|
+      next if ignore_values.any? { |ignore_value| key =~ ignore_value }
+      any_blank = value.is_a?(Hash) ? deep_blank?(value, ignore_values) : value.blank?
+      return false unless any_blank
     end
+    true
   end
 end
+
+
--- test/reject_deeply_nested_test.rb
+++ test/reject_deeply_nested_test.rb
@@ -14,7 +14,7 @@ class RejectDeeplyNestedTest < Minitest::Test
       k: { l: '', m: { n: { o: '' } } },
       p: { q: '', r: '', s: { t: { u: { w: '' } } } }
     }
-    assert ::RejectDeeplyNested::DeeplyNested.deep_blank?(deep_hash_witout_values)
+    assert ::RejectDeeplyNested::BLANK.call(deep_hash_witout_values)
   end
 
   def test_return_false_for_nested_hash_with_at_least_one_non_blank_value
@@ -26,7 +26,7 @@ class RejectDeeplyNestedTest < Minitest::Test
       k: { l: '', m: { n: { o: "some_passed_value" } } },
       p: { q: '', r: '', s: { t: { u: { w: '' } } } }
     }
-    refute ::RejectDeeplyNested::DeeplyNested.deep_blank?(deep_hash_with_value)
+    refute ::RejectDeeplyNested::BLANK.call(deep_hash_with_value)
   end
 
   def test_ignore_value_if_key_equals_destroy
@@ -38,7 +38,43 @@ class RejectDeeplyNestedTest < Minitest::Test
       k: { l: '', m: { n: { '_destroy' => "some_passed_value" } } },
       p: { q: '', r: '', s: { t: { u: { w: '' } } } }
     }
-    assert ::RejectDeeplyNested::DeeplyNested.deep_blank?(deep_hash_with_value)
+    assert ::RejectDeeplyNested::BLANK.call(deep_hash_with_value)
+  end
+
+  def test_smart_blank_which_can_ignore_passed_values
+    deep_hash_with_value = {
+      a: '',
+      b: '',
+      c: { d: '', e: '' },
+      f: { g: '', h: { j: '' } },
+      k: { l: '', m: { n: { iddqd_id: "some_passed_value" } } },
+      p: { q: '', r: '', s: { t: { u: { w: '' } } } }
+    }
+    assert ::RejectDeeplyNested::SMART_BLANK.curry.([/_id$/]).call(deep_hash_with_value)
+  end
+
+  def test_smart_blank_which_can_ignore_passed_values_but_not_forget_about_destroy_key
+    deep_hash_with_value = {
+      a: '',
+      b: '',
+      c: { d: '', e: '' },
+      f: { g: '', h: { j: '' } },
+      k: { l: '', m: { n: { iddqd_id: "some_passed_value" } } },
+      p: { q: '', r: '', s: { t: { u: { "_destroy" => '' } } } }
+    }
+    assert ::RejectDeeplyNested::SMART_BLANK.curry.([/_id$/]).call(deep_hash_with_value)
+  end
+
+  def test_smart_blank_which_return_false_for_hash_with_non_ignored_value
+    deep_hash_with_value = {
+      a: '',
+      b: '',
+      c: { d: '', e: '' },
+      f: { g: '', h: { j: '' } },
+      k: { l: '', m: { n: { iddqd_id: "some_passed_value" } } },
+      p: { q: '', r: '', s: { t: { u: { "_destroy" => '' } } } }
+    }
+    refute ::RejectDeeplyNested::SMART_BLANK.curry.([]).call(deep_hash_with_value)
   end
 end
 
@@ -50,39 +86,3 @@ end
 
 
 
-
-
-
-# describe DeeplyNested do
-#   subject { DeeplyNested::BLANK }
-
-#   let(:deep_hash_witout_values) do
-#     {
-#       a: '',
-#       b: '',
-#       c: { d: '', e: '' },
-#       f: { g: '', h: { j: '' } },
-#       k: { l: '', m: { n: { o: '' } } },
-#       p: { q: '', r: '', s: { t: { u: { w: '' } } } }
-#     }
-#   end
-
-#   let(:deep_hash_with_value) do
-#     {
-#       a: '',
-#       b: '',
-#       c: { d: '', e: '' },
-#       f: { g: '', h: { j: '' } },
-#       k: { l: '', m: { n: { o: "some_passed_value" } } },
-#       p: { q: '', r: '', s: { t: { u: { w: '' } } } }
-#     }
-#   end
-
-#   it "return false if on even one level of nested hash filled value" do
-#     expect(subject.call(deep_hash_with_value)).to be false
-#   end
-
-#   it "return true if all levels of nested hash are blank" do
-#     expect(subject.call(deep_hash_witout_values)).to be true
-#   end
-# end
